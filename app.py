1cca5faaf8d20a5a39e736265da5cd527520385b
Jinny Viboonlarp <jviboonlarp@brandeis.edu>
2022-07-06 18:45:33 +0700

    add relation extraction

--- app.py
+++ app.py
@@ -37,8 +37,10 @@ from mmif.serialize import Mmif
 from mmif.vocabulary import AnnotationTypes, DocumentTypes
 from lapps.discriminators import Uri
 
-# Define Uri_NEL since it is not on lappsgrid.org yet
+# Define Uri_NEL and Uri_DEP since it is not on lappsgrid.org yet
 Uri_NEL = "http://vocab.lappsgrid.org/LinkedNamedEntity"
+Uri_DEP = "http://vocab.lappsgrid.org/SyntacticRelation"
+Uri_NELR = "http://vocab.lappsgrid.org/LinkedNamedEntityRelation"
 
 APP_VERSION = '0.0.8'
 APP_LICENSE = 'Apache 2.0'
@@ -71,6 +73,7 @@ class NEL_App(ClamsApp):
 
         metadata.add_input(Uri.NE)
         metadata.add_output(Uri_NEL)
+        metadata.add_output(Uri_NELR)
         return metadata
 
     def _annotate(self, mmif, **kwargs):
@@ -99,7 +102,8 @@ class NEL_App(ClamsApp):
     def _new_view(self, docid=None):
         view = self.mmif.new_view()
         self.sign_view(view)
-        view.new_contain(Uri_NEL, document=docid)
+        for attype in (Uri_NEL, Uri_NELR):
+            view.new_contain(attype, document=docid)
         return view
 
     def _add_tool_output(self, old_view, new_view, view_id=None):
@@ -116,8 +120,9 @@ class NEL_App(ClamsApp):
         repo = site.data_repository()
 
         # for each NER-type annotation, use the text to search wikidata
+        interested_entities = collections.defaultdict(dict) # this is a dict to store information of entities with wikidata information
         interested_keys = ['url','label','description']
-        annotation_property_order = ['text','label','category','description','wikidata_id','url','id']
+        annotation_property_order = ['root_i','text','label','category','description','wikidata_id','url','id']
         for annotation in old_view['annotations']:
             if(annotation.at_type == Uri.NE):
                 doc_id = annotation['properties']['document'] if "document" in annotation['properties'] else None
@@ -127,7 +132,8 @@ class NEL_App(ClamsApp):
                 firstEntry = wikidataEntries["search"][0] if (len(wikidataEntries["search"])>0) else None
                 if(firstEntry != None):
                     # create new annotation from the old annotation plus the data from wikidata
-                    properties = { "text": annotation['properties']['text'], "category": annotation['properties']['category']}
+                    properties = { "text": annotation['properties']['text'], "category": annotation['properties']['category'], \
+                                   "root_i": annotation['properties']['root_i']}
                     if 'id' in firstEntry:
                         properties['wikidata_id'] = firstEntry['id']
                     for key in interested_keys:
@@ -138,15 +144,52 @@ class NEL_App(ClamsApp):
                         new_view, Uri_NEL, Identifiers.new("nel"),
                         doc_id, annotation['properties']['start'], annotation['properties']['end'],
                         properties)
+                    # store doc_id and root_i in a dict to search later for meaningful relations between named entities with
+                    # wikidata entries
+                    doc_id = annotation['properties']['document'] if "document" in annotation['properties'] else 0
+                    entity_id = annotation['properties']['root_i']
+                    interested_entities[doc_id][entity_id] = properties
                     
+        # store syntactic relations in the old view (so that they could be searched later
+        child_to_head = collections.defaultdict(dict)
+        for annotation in old_view['annotations']:
+            if(annotation.at_type == Uri_DEP):
+                doc_id = annotation['properties']['document'] if "document" in annotation['properties'] else 0
+                child_i = annotation['properties']['child_i']
+                child_to_head[doc_id][child_i] = {key: annotation['properties'][key] for key in ['dep','head_text','head_lemma','head_i']}
+
+        # search for named entities with the same head texts
+        for doc_id in interested_entities:
+            for i1 in interested_entities[doc_id]: # iterate over root_i of named entities in this document
+                for i2 in interested_entities[doc_id]:
+                    if ((i1 < i2) and (i1 in child_to_head[doc_id]) and (i2 in child_to_head[doc_id]) and \
+                        (child_to_head[doc_id][i1]["head_i"] == child_to_head[doc_id][i2]["head_i"])):
+                        properties = { "e1_text": interested_entities[doc_id][i1]['text'], "e1_label": interested_entities[doc_id][i1]['label'], \
+                                    "e1_root_i": interested_entities[doc_id][i1]['root_i'], "e1_wikidata_id": interested_entities[doc_id][i1]['wikidata_id'], \
+                                    "e1_dep": child_to_head[doc_id][i1]['dep'], \
+                                        "e2_text": interested_entities[doc_id][i2]['text'], "e2_label": interested_entities[doc_id][i2]['label'], \
+                                    "e2_root_i": interested_entities[doc_id][i2]['root_i'], "e2_wikidata_id": interested_entities[doc_id][i2]['wikidata_id'], \
+                                    "e2_dep": child_to_head[doc_id][i2]['dep'], \
+                                        "rel_text": child_to_head[doc_id][i1]['head_text'], "rel_lemma": child_to_head[doc_id][i1]['head_lemma'], \
+                                    "rel_i": child_to_head[doc_id][i1]['head_i']}
+                        doc_id_for_annotation = doc_id if (doc_id != 0) else None
+                        if((view_id != None) and (doc_id_for_annotation != None)):
+                            doc_id_for_annotation = view_id + ':' + doc_id_for_annotation
+                        add_annotation(
+                            new_view, Uri_NELR, Identifiers.new("nelr"),
+                            doc_id_for_annotation, None, None,
+                            properties)
+        
 
 def add_annotation(view, attype, identifier, doc_id, start, end, properties):
     """Utility method to add an annotation to a view."""
     a = view.new_annotation(attype, identifier)
     if doc_id is not None:
         a.add_property('document', doc_id)
-    a.add_property('start', start)
-    a.add_property('end', end)
+    if start is not None:
+        a.add_property('start', start)
+    if end is not None:
+        a.add_property('end', end)
     for prop, val in properties.items():
         a.add_property(prop, val)
 
